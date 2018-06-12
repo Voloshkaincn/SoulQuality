5cceb8d9531dad0bb41c3f4c9ad0a145eb63e532
Voloshkaincn <barvinok.cn@gmail.com>
2018-06-12 16:20:40 +0300

    change  bootstrap and some fixes

--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
 
     $('.portfolio-slider').slick({
           arrows: true,
+          appendArrows: '.portfolio-nav-bottom .portfolio-buttons',
           dots: false,
           infinite: true,
           speed: 300,
@@ -115,7 +116,7 @@ $(document).ready(function() {
         //Navigation
         menu: '',
         lockAnchors: false,
-        anchors:['1stPage', '2ndPage','3rdPage','4thPage','5thPage','6thPage','footer'],
+        anchors:['home', 'services','portfolio','ourTeam','advantages','contacts','footer'],
         navigation: true,
         navigationPosition: 'left',
         navigationTooltips: ['HOME', 'SERVICES', 'PORTFOLIO', 'OUR TEAM', 'ADVANTAGES', 'CONTACTS', 'button'],
@@ -162,28 +163,27 @@ $(document).ready(function() {
         slideSelector: '.slide',
 
         //events
-        onLeave: function(index, nextIndex, direction){},
+        onLeave: function(index, nextIndex, direction){
+          if(index == 3){
+            $('.stone').addClass('stone-anim');
+          }
+          if((index == 2 && direction == 'down') || (index == 4 && direction == 'up')){
+            $('.stone').removeClass('stone-anim');
+          }
+        },
         afterLoad: function(anchorLink, index){
-          if(index == 4){
-            // new Vivus('des_skill_svg', {duration: 100});
-            // new Vivus('des-skill-svg-2', {duration: 100});
-            // new Vivus('des-skill-svg-3', {duration: 100});
-            // new Vivus('des-skill-svg-4', {duration: 100});
-            // new Vivus('brackets-skill-svg', {duration: 100});
-            // new Vivus('brackets-skill-svg-2', {duration: 100});
-            // new Vivus('php-skill-svg', {duration: 100});
-            // new Vivus('php-skill-svg-2', {duration: 100});
+          if(anchorLink == 'portfolio'){
+            $('.stone').removeClass('stone-anim');
           }
         },
         afterRender: function(){},
         afterResize: function(){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     });
 
     $("#fp-nav ul li:last-child").click(function(e) {
         e.preventDefault();
         $("#fp-nav").toggleClass('hidden-menu');
+        $(".section").toggleClass('pl-0')
     });
 });
 
